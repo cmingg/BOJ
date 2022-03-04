e29186babf208a9c93fc76a12b363d88a0398056
cmingg <87798963+cmingg@users.noreply.github.com>
2022-03-04 12:20:01 +0900

    아스키 코드

---
+++ 문자열/11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.next().charAt(0);	// 문자열 -> 문자 변환
+		
+		System.out.println(n);
+	}
+}
