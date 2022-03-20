b31d5459d779bb4aba492870ede4cfdb96c3707a
cmingg <chooo12343@gmail.com>
2022-03-20 20:54:58 +0900

    팩토리얼

---
+++ 재귀/10872.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    // 팩토리얼
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(factorial(n));
+    }
+
+    static int factorial(int n){
+
+        if(n == 0){
+            return 1;
+        }else{
+            return n * factorial(n-1);
+        }
+    }
+
+}
